f0fd74c4ce7a7d53968ec54cbc1f0f4c0eb874fb
Kevin Lam <mtn013209@gmail.com>
2023-01-11 11:17:06 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
@@ -8,20 +7,6 @@ function App() {
       <a href="https://kevin-lambda.github.io/p5-async-week-project/testWithExtension/index.html">
         p5-async-week-project
       </a>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   )
 }
